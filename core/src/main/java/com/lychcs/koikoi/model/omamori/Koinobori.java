ef20f9889408816618981d85d1c5f6c90137704a
ly68ee <manuelturin1706@gmail.com>
2026-09-12 19:00:34 +0200

    patched koinobori omamori

--- core/src/main/java/com/lychcs/koikoi/model/omamori/Koinobori.java
+++ core/src/main/java/com/lychcs/koikoi/model/omamori/Koinobori.java
@@ -1,4 +1,5 @@
 package com.lychcs.koikoi.model.omamori;
+import com.lychcs.koikoi.scoring.HandContext;
 import com.lychcs.koikoi.scoring.ScoreAccumulator;
 import com.lychcs.koikoi.scoring.ScoreContext;
 
@@ -15,15 +16,7 @@ public class Koinobori implements Omamori {
 
     @Override
     public boolean evaluate(ScoreContext context, ScoreAccumulator acc) {
-        // Feuert erst, sobald Koi-Koi (Risk) größer als 1.0 ist
-        if (context.koiKoiMult() > 1.0) {
-            int extraChips = 100;
-            int extraMult = (int) (context.koiKoiMult() * 3);
-
-            acc.addChips("Upstream Swim (Chips)", extraChips);
-            acc.addMult("Upstream Swim (Mult)", extraMult);
-            return true;
-        }
+        //???
         return false;
     }
 }
